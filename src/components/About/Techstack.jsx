f5ec8d825a873102c16216e0094ec62dea2f10ac
bennyv8 <bvanhr@gmail.com>
2023-07-31 13:37:50 -0700

    Updated tech stack with on-hover text bubbles

--- src/components/About/Techstack.jsx
+++ src/components/About/Techstack.jsx
@@ -7,38 +7,62 @@ import {
   DiMongodb,
   DiPython,
   DiGit,
+  DiHtml5,
+  DiCss3
 } from "react-icons/di";
 import {
   SiRedis,
   SiPostgresql,
+  SiJest,
 } from "react-icons/si";
+import "./About.css";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">JavaScript</div>
         <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Node.Js</div>
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">React</div>
         <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Python</div>
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">MongoDb</div>
         <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">PostgreSQL</div>
         <SiPostgresql />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
+        <div className="text-bubble">Git</div>
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Redis</div>
+        <SiRedis />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Html</div>
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">CSS</div>
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Jest</div>
+        <SiJest />
       </Col>
     </Row>
   );
