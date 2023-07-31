5710cea49d40ac520ac092cc12f50e6535ae03d0
bennyv8 <bvanhr@gmail.com>
2023-07-31 13:38:09 -0700

    Updated tool stack with on-hover text bubbles

--- src/components/About/Toolstack.jsx
+++ src/components/About/Toolstack.jsx
@@ -4,19 +4,44 @@ import {
   SiVisualstudiocode,
   SiPostman,
   SiSlack,
+  SiAmazonaws,
 } from "react-icons/si";
+import {
+  DiDocker,
+  DiGoogleDrive,
+  DiLinux
+} from "react-icons/di";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Visual Studio Code</div>
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Postman</div>
         <SiPostman />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Docker</div>
+        <DiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">AWS</div>
+        <SiAmazonaws />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Linux</div>
+        <DiLinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Slack</div>
         <SiSlack />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <div className="text-bubble">Google Drive</div>
+        <DiGoogleDrive />
       </Col>
     </Row>
   );
