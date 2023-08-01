d2ba1e8680e2a04fb419389514b085fc6b0c8a0d
bennyv8 <bvanhr@gmail.com>
2023-08-01 16:31:00 -0700

    update 5

--- deployment/app_stop.sh
+++ deployment/app_stop.sh
@@ -1,8 +1,1 @@
 # Check if a Vite process is running
-if pgrep -x "vite" > /dev/null; then
-  # Vite process exists, so terminate it
-  pkill -f "vite"
-  echo "Vite process has been terminated."
-else
-  echo "No Vite process is currently running."
-fi
